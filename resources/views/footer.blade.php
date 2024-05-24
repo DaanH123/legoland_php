0588f7adc27b585e4864713f466f49c75283e14c
DaanH123 <daan.hendriksen1@student.graafschapcollege.nl>
2024-05-24 14:13:51 +0200

    Fix for footer changing colors

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-white shadow bg-yellow-400">
+<footer class="shadow bg-yellow-400">
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="#" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
